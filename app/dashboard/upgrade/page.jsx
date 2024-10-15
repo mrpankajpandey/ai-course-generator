1537186ddd1498f8149d9ed91d7f01b8bd83f24a
pa1narendra <pavannarendra2002@gmail.com>
2024-10-15 18:32:18 +0530

    corrected the sentence in upgrage page

--- app/dashboard/upgrade/page.jsx
+++ app/dashboard/upgrade/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Upgrade = () => {
   return (
     <>
-           <h2 className='font-medium text-lg mb-3'>Reach out to me , I'll promoted you as admin , So you can created a unlimted course with Ai</h2>
+           <h2 className='font-medium text-lg mb-3'>Reach out to me , I'll promote you as an admin , So you can create unlimited courses with AI</h2>
       <div className="relative w-full h-screen overflow-hidden">
         <iframe className='absolute inset-0 w-full h-full border-none overflow-hidden'
         src="https://buymeacoffee.com/mrpankajpandey_"
